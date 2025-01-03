ecb9ec66ffed7e764e6cc4b4f6975dde576da42b
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-03 14:51:45 +0500

    remove unnecessary console log from CreateBlogForm component

--- src/components/blog/CreateBlogForm.jsx
+++ src/components/blog/CreateBlogForm.jsx
@@ -136,8 +136,6 @@ const CreateBlogForm = ({ user }) => {
     );
   };
 
-  console.log(title, category, coverImage, content);
-
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
       <header className="flex justify-between items-center mb-8">
